ec06f9e55036ee6c0b200c308649fb7da51fcea9
daryle <daryle.tan@gmail.com>
2023-07-05 00:08:26 -0500

    game 1-5 fin

--- contracts/Game1.sol
+++ contracts/Game1.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
+import "hardhat/console.sol";
 
 contract Game1 {
-  event Winner(address winner);
+    event Winner(address winner);
 
-  function win() public {
-    emit Winner(msg.sender);
-  }
+    function win() public {
+        emit Winner(msg.sender);
+    }
 }
